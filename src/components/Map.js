46f6e107d4c82a5b013731aa7036ce019a7a5d16
Trong Dao <trongdao20402@gmail.com>
2024-10-16 10:49:49 +0700

    init fe

---
+++ src/components/Map.js
@@ -0,0 +1,38 @@
+import React from "react";
+import GoogleMapReact from "google-map-react";
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+export const Map = () => {
+  const defaultProps = {
+    center: {
+      lat: 20.99,
+      lng: 105.77,
+    },
+    zoom: 11,
+  };
+
+  return (
+    <div
+      style={{
+        height: "300px",
+        width: "100%",
+        border: "20px",
+        // filter: " brightness(2) ",
+      }}
+    >
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyCSeYQYg5k7fVzBA4L_X9rmyGMpB-pKhlc" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={20.99}
+          lng={105.77}
+          text={<span style={{ fontSize: "24px" }}>📍</span>}
+        />
+      </GoogleMapReact>
+    </div>
+  );
+};
+export default Map;
